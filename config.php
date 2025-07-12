8d2697b273ed00b523d03d5bbaf5e9c679d1f5f0
yugpatel2907 <patelyug8901@gmail.com>
2025-07-12 16:28:55 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+define('GOOGLE_CLIENT_ID', '782187096910-0tgvb0b66mo88kk9nqgeut8q1b3kluo8.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_REDIRECT_URI', 'http://localhost/hackathon_db/callback.php');
